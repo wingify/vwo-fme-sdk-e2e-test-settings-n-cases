7d1077d67088eb1be959e45cee9c273cff167250
softvar <varun2902@gmail.com>
2024-05-24 14:17:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## VWO FME SDK Test Settings and Cases
+
+VWO Feature Management and Experimentation - SDK Test Settings and Cases at one place
+
+## Authors
+
+- [Varun Malhotra](https://github.com/softvar)([@s0ftvar](https://twitter.com/s0ftvar))
+
+## Contributing
+
+Please go through our [contributing guidelines](https://github.com/wingify/vwo-fme-sdk-e2e-test-settings-n-cases/blob/master/CONTRIBUTING.md)
+
+
+## Code of Conduct
+
+[Code of Conduct](https://github.com/wingify/vwo-fme-sdk-e2e-test-settings-n-cases/blob/master/CODE_OF_CONDUCT.md)
+
+## License
+
+[Apache License, Version 2.0](https://github.com/wingify/vwo-fme-sdk-e2e-test-settings-n-cases/blob/master/LICENSE)
+
+Copyright 2024 Wingify Software Pvt. Ltd.
---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  BASIC_ROLLOUT_SETTINGS: require('./src/settings/OnlyRolloutSettings.json'),
+  BASIC_ROLLOUT_TESTING_RULE_SETTINGS: require('./src/settings/RolloutAndTestingSettings.json'),
+  NO_ROLLOUT_ONLY_TESTING_RULE_SETTINGS: require('./src/settings/NoRolloutAndOnlyTestingSettings.json'),
+  ROLLOUT_TESTING_PRE_SEGMENT_RULE_SETTINGS: require('./src/settings/RolloutAndTestingSettingsWithPreSegment.json'),
+  TESTING_WHITELISTING_SEGMENT_RULE_SETTINGS: require('./src/settings/SettingsWithWhitelisting.json'),
+  MEG_CAMPAIGN_RANDOM_ALGO_SETTINGS: require('./src/settings/MegRandomAlgoCampaignSettings.json'),
+  MEG_CAMPAIGN_ADVANCE_ALGO_SETTINGS: require('./src/settings/MegAdvanceAlgoCampaignSettings.json'),
+
+  TEST_DATA: require('./src/test-cases/index.json'),
+};
